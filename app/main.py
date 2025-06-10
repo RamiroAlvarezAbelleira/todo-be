ab9c6030b87293825523acabe067d8451f59aaa3
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-06-10 23:49:06 +0200

    connection with mongo

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from app.database.mongo import client
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/ping-mongo")
+async def ping_mongo():
+    try:
+        await client.admin.command("ping")
+        return {"message": "MongoDB is reachable"}
+    except Exception as e:
+        return {"error": str(e)}
