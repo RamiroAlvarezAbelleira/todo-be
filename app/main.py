ddd84801c135369f4ef9830c1acf813e15c62d87
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-06-15 19:31:34 +0200

    new cors config

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from app.database.mongo import client
 from app.routers import todo_lists, tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Orígenes permitidos
+origins = [
+    "http://localhost:3000",  # Next.js
+    "http://127.0.0.1:3000",  # Por si usás esta dirección también
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,              # Permite esos orígenes
+    allow_credentials=True,
+    allow_methods=["*"],                # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],                # Permite todos los headers
+)
+
+\
 app.include_router(todo_lists.router, prefix="/api", tags=["Todo Lists"])
 app.include_router(tasks.router, prefix="/api", tags=["Tasks"])
 
